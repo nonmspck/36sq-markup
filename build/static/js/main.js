ddbd9f19bba585db2e104e0e18a3ab2ebb585dc1
huness <tialdorf@gmail.com>
2018-11-03 18:12:25 +0300

    +full header menu and nav

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -60,4 +60,21 @@ $(document).ready(function () {
 
 "use strict";
 "use strict";
+
+$(document).ready(function () {
+    $(".headerMenu__btn-open").click(function (event) {
+        event.preventDefault();
+        $(".headerMenu__btn-open").addClass("headerMenu__btn-jsHide");
+        $(".headerMenu__btn-close").removeClass("headerMenu__btn-jsHide");
+        $(".headerMenu__wrap").removeClass("headerMenu__wrap-jsHide");
+        $(".socials").removeClass("socials-jsHide");
+    });
+    $(".headerMenu__btn-close").click(function (event) {
+        event.preventDefault();
+        $(".headerMenu__btn-close").addClass("headerMenu__btn-jsHide");
+        $(".headerMenu__btn-open").removeClass("headerMenu__btn-jsHide");
+        $(".headerMenu__wrap").addClass("headerMenu__wrap-jsHide");
+        $(".socials").addClass("socials-jsHide");
+    });
+});
 "use strict";
--- markup/components/headerNav/headerNav.js
+++ markup/components/headerNav/headerNav.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+    $(".headerMenu__btn-open").click(function(event) {
+        event.preventDefault();
+        $(".headerMenu__btn-open").addClass("headerMenu__btn-jsHide");
+        $(".headerMenu__btn-close").removeClass("headerMenu__btn-jsHide");
+        $(".headerMenu__wrap").removeClass("headerMenu__wrap-jsHide");
+        $(".socials").removeClass("socials-jsHide");
+    });
+    $(".headerMenu__btn-close").click(function(event) {
+        event.preventDefault();
+        $(".headerMenu__btn-close").addClass("headerMenu__btn-jsHide");
+        $(".headerMenu__btn-open").removeClass("headerMenu__btn-jsHide");
+        $(".headerMenu__wrap").addClass("headerMenu__wrap-jsHide");
+        $(".socials").addClass("socials-jsHide");
+    });
+});
