a7848c5d7c4575bf7a604e194428b0a065a656ff
huness <tialdorf@gmail.com>
2018-11-04 18:36:45 +0300

    +slider without arrows, counter

--- markup/components/welcomeSlider/welcomeSlider.js
+++ markup/components/welcomeSlider/welcomeSlider.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+    $('.textSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        infinite: false
+    });
+});
