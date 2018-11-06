764a2d82ff35051593c878548f6e52e8fa55fd63
huness <tialdorf@gmail.com>
2018-11-06 19:30:24 +0300

    +intro section

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -80,6 +80,21 @@ $(document).ready(function () {
     });
 });
 "use strict";
+"use strict";
+
+var $introSlider = $(".introSlider__slider");
+
+$introSlider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // fade: true,
+    // cssEase: 'linear',
+    draggable: false,
+    infinite: false,
+    prevArrow: ".introSlider__control-prev",
+    nextArrow: ".introSlider__control-next"
+});
+"use strict";
 
 $(document).ready(function () {
     var $welcomeSlider = $(".textSlider");
